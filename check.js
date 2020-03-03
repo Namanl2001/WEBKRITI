95b76929cf78e8de11b1268147826e2b8aaa5fba
Namanl2001 <namanlakhwani@gmail.com>
2020-03-04 02:21:20 +0530

    naman

--- check.js
+++ check.js
@@ -11,13 +11,7 @@ function checkdate()
 	var date=signup.day.value;
 	var month=signup.month.value;
 	var year=signup.year.value;
-	if(date==0)
-	alert("Select a correct date ");
-	else if (month==0)
-	alert("Select a correct month ");
-	else if (year==0)
-	alert("Select a valid year ");
-	else if(date>=30 && month==2)
+	if(date>29 && month==2)
 	alert("Select a valid date ");
 	else if (date==31&&(month==6||month==11||month==4||month==9))
 	alert("Select a valid date ");
